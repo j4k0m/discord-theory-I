8a34e6f74982ad16851e1056bf13b7d14af7b683
Jakom <48088579+RyouYoo@users.noreply.github.com>
2021-09-17 20:44:53 +0100

    Create generate_hex.py

---
+++ generate_hex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import random
+
+_map = [3, 4, 5, 7, 8, 9]
+
+def generate_map():
+    e = random.choice(_map)
+    if e >= 3 and e <= 5:
+        n = _map[::-1][0:3][_map[0:3].index(e)]
+    else:
+        n = _map[0:3][_map[::-1][0:3].index(e)]
+    return {"Extanded": e, "Normal": n}
+
+def generate():
+    c = generate_map()
+    ex, no = c["Extanded"], c["Normal"]
+    _chars = random.sample(range(128,255), ex)
+    _chars.extend(random.sample(range(1,126), no))
+    random.shuffle(_chars)
+    return " ".join(list(map(hex ,_chars)))
+
+print(generate())
